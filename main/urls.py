92f77bd7280115dc6c4a3a602ebb5aedf0bdbfbf
akshaykskrn <134961550+akshaykskrn@users.noreply.github.com>
2024-09-11 12:21:27 +0530

    edited added product

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add/', views.add_numbers, name='add_numbers'),
+    path('pnr/', views.simple_interest,name='simple_intrest'),
+    path('mul/', views.product,name='product'),
 ]
--- main/views.py
+++ main/views.py
@@ -16,3 +16,35 @@ def add_numbers(request):
             error = "Something went wrong"
 
     return render(request, 'add.html', {'result': result, 'error': error}) 
+
+def simple_interest(request):
+    result = None
+    error = None
+
+    if request.method == "POST":
+        p=request.POST.get('p')
+        n=request.POST.get('n')
+        r=request.POST.get('r')
+
+        try:
+            simple=(float(p) * float(n) * float(r))/100
+            result = f"The sum is:{simple}"
+        except:
+            error = "Something went wrong"
+    return render(request,'pnr.html',{'result':result,'error':error})
+
+def product(request):
+    result = None
+    error = None
+
+    if request.method == "POST":
+        p=request.POST.get('p')
+        n=request.POST.get('n')
+        r=request.POST.get('r')
+
+        try:
+            simple=(float(p) * float(n) * float(r))
+            result = f"The product is:{simple}"
+        except:
+            error = "Something went wrong"
+    return render(request,'mul.html',{'result':result,'error':error})
